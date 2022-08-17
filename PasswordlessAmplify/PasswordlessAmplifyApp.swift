f41bee7d47d66d4838290276d1dbcce0b7cd1e27
miketran <miketran@amazon.com>
2022-08-17 13:14:34 -0700

    Initial Commit

---
+++ PasswordlessAmplify/PasswordlessAmplifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordlessAmplifyApp.swift
+//  PasswordlessAmplify
+//
+//  Created by Tran, Michael on 8/17/22.
+//
+
+import SwiftUI
+
+@main
+struct PasswordlessAmplifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
